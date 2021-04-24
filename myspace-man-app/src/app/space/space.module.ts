f791f7a5a4a24acc343ef4ad7279b04dde525c9d
Mateusz Sorys <m.sorys@i4b.pl>
2021-04-24 11:37:23 +0200

    dodanie modułu space i  komponentu hangaru

---
+++ myspace-man-app/src/app/space/space.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SpaceRoutingModule } from './space-routing.module';
+import { HangarComponent } from './hangar/hangar.component';
+
+
+@NgModule({
+  declarations: [
+    HangarComponent
+  ],
+  exports: [
+    HangarComponent
+  ],
+  imports: [
+    CommonModule,
+    SpaceRoutingModule
+  ]
+})
+export class SpaceModule { }
